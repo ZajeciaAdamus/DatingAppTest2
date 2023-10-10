206bf2ff0824e73ba360ebca4d2bcb924f2ad516
Paweł Adamus <zajecia.adamus@gmail.com>
2023-10-10 12:26:56 +0200

    1st extension service added

--- API/Program.cs
+++ API/Program.cs
@@ -1,20 +1,32 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
 
-builder.Services.AddScoped<ITokenService,TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true, //serwer sprawdza klucze
+                        IssuerSigningKey = new SymmetricSecurityKey(    
+                            Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+                            ValidateIssuer = false,
+                            ValidateAudience = false
+                    };
+                });
 
 
-builder.Services.AddDbContext<DataContext>(opt =>
-opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
 
 builder.Services.AddControllers();
 
@@ -22,6 +34,9 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -8,12 +8,10 @@ using Microsoft.IdentityModel.Tokens;
 namespace API.Services;
 public class TokenService : ITokenService
 {
-
     private readonly SymmetricSecurityKey _key;
-
     public TokenService(IConfiguration config)
     {
-        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token key"]));
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
     }
     public string CreateToken(AppUser user)
     {
